9e32ff5c00d7bf61310a724cc2989036921e73c7
belie <belie@medifit.com>
2015-03-03 18:22:17 -0500

    added in some code based on the meeting. (may still break things)

--- src/bizzfuzzUI/models.py
+++ src/bizzfuzzUI/models.py
@@ -4,10 +4,18 @@ from django.db import models
 from datetime import date, datetime
 from random import randrange
 from django.utils import timezone
+import uuid
 
 class User(models.Model):
     birthdate = models.DateField('someones birthday')
     random_number = models.IntegerField('bizzbuzz randomly generated number')
+    username = models.CharField(max_length=50, blank=True, null=True)
+    password = models.CharField(max_length=150, blank=True, null=True)
+    email_address = models.CharField(max_length=255, blank=True, null=True)
+    firstname = models.CharField(max_length=55, blank=True, null=True)
+    lastname = models.CharField(max_length=75, blank=True, null=True)
+    forgot_password_date = models.DateTimeField('The date someone initiates the password request', null=True)
+    forgot_password_code = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.random_number
@@ -26,4 +34,14 @@ class User(models.Model):
         return self.birthdate.strftime('%m/%d/%Y')
 
     def set_random_number(self):
-        self.random_number = randrange(1,100)
+        self.random_number = randrange(1, 100)
+
+    def set_forgot_password(self):
+        self.forgot_password_date = datetime.now()
+        self.forgot_password_code = uuid.uuid1()
+
+    # Checks to see if the password request is still valid; within a date/time range and if the code has been used
+    def is_password_request_valid(self):
+        if self.forgot_password_code == '':
+            return False
+        return True
