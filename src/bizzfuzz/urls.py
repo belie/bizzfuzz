92029e0be915e6940f34771fb330eab14c3acfbc
belie <belie@medifit.com>
2015-03-03 15:46:08 -0500

    Allow top level to resolve to a new URL. Started Login / Forgot Password feature.

--- src/bizzfuzz/urls.py
+++ src/bizzfuzz/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from bizzfuzzUI.views import UserListView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bizzfuzz.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', UserListView.as_view(), name='list_user'),
     url(r'^bizzfuzz/', include('bizzfuzzUI.urls', namespace="bizzfuzz")),
     url(r'^admin/', include(admin.site.urls)),
 )
